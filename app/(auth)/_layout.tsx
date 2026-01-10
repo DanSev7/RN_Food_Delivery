306b7ecdbe3bf2ab4f3d00948966ecc3c2e87417
DanSev7 <danielayele077@gmail.com>
2026-01-10 16:32:19 +0300

    Routing

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Slot } from 'expo-router'
+import React from 'react'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const AuthLayout = () => {
+  return (
+    <SafeAreaView>
+      <Slot />
+    </SafeAreaView>
+  )
+}
+
+export default AuthLayout
